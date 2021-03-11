28699b1b038fd7c6e1ff0b662398b91d594fc60c
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2021-03-11 12:21:17 -0500

    touch ups completed

---
+++ client/src/components/PlantList.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class PlantList extends Component {
+  // add state with a property called "plants" - initialize as an empty array
+
+  // when the component mounts:
+  //   - fetch data from the server endpoint - http://localhost:3333/plants
+  //   - set the returned plants array to this.state.plants
+
+  /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
+  constructor(){
+    super();
+    this.state = {
+        plants:[]
+    }
+  }
+  componentDidMount(){
+    axios.get("http://localhost:3333/plants")
+      .then((res)=>{
+        this.setState([
+          ...this.state.plants, this.state.plants = res.data.plantsData
+        ])
+        console.log(this.state.plants)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+  }
+  render() {
+    return (
+      <main className="plant-list">
+         {this.state.plants.map((plant) => (
+          <div data-testid="plant-card" className="plant-card" key={plant.id}>
+            <img className="plant-image" src={plant.img} alt={plant.name} />
+            <div className="plant-details">
+              <h2 className="plant-name">{plant.name}</h2>
+              <p className="plant-scientific-name">{plant.scientificName}</p>
+              <p>{plant.description}</p>
+              <div className="plant-bottom-row">
+                <p>${plant.price}</p>
+                <p>☀️ {plant.light}</p>
+                <p>💦 {plant.watering}x/month</p>
+              </div>
+              <button
+                className="plant-button"
+                onClick={() => this.props.addToCart(plant)}
+              >
+                Add to cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </main>
+      
+    );
+  }
+}
